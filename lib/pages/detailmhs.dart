44959b99b62832afbc3981417219b0c2eb8e0933
Milzan Malik <milzan_malik@outlook.com>
2021-07-27 12:55:21 +0700

    fix error

---
+++ lib/pages/detailmhs.dart
@@ -0,0 +1,367 @@
+import 'package:flutter/material.dart';
+import 'package:logbook_management/db/db_mhs.dart';
+import 'package:logbook_management/models/mahasiswa.dart';
+import 'package:logbook_management/utils/constants.dart';
+import 'package:flutter_icons/flutter_icons.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:fluttertoast/fluttertoast.dart';
+import 'package:flutter/services.dart';
+
+// ignore: must_be_immutable
+class Detailmhs extends StatelessWidget {
+  final Mahasiswa mhs;
+  Detailmhs({Key key, this.mhs}) : super(key: key);
+
+  var txtnim = TextEditingController();
+  var txtnama = TextEditingController();
+  var txtkelas = TextEditingController();
+  var txtprodi = TextEditingController();
+
+  void setText() {
+    txtnim.text = mhs.nim;
+    txtnama.text = mhs.namaLengkap;
+    txtkelas.text = mhs.kelas;
+    txtprodi.text = mhs.prodi;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    Future<void> _showMyDialog(String _tittle, String _msg) async {
+      return showDialog<void>(
+        context: context,
+        barrierDismissible: false, // user must tap button!
+        builder: (BuildContext context) {
+          return AlertDialog(
+            title: Text(_tittle),
+            content: SingleChildScrollView(
+              child: ListBody(
+                children: <Widget>[
+                  Text(_msg),
+                ],
+              ),
+            ),
+            actions: <Widget>[
+              TextButton(
+                child: const Text('Tidak'),
+                onPressed: () {
+                  Navigator.of(context).pop();
+                },
+              ),
+              TextButton(
+                child: const Text('Ya'),
+                onPressed: () async {
+                  await MhsDatabase.instance.delete(mhs.id);
+                  Navigator.of(context).pop();
+                  Navigator.of(context).pop();
+                  Fluttertoast.showToast(
+                      msg: "Data Berhasil Dihapus",
+                      toastLength: Toast.LENGTH_SHORT,
+                      gravity: ToastGravity.CENTER,
+                      timeInSecForIosWeb: 1,
+                      fontSize: 16.0);
+                },
+              ),
+            ],
+          );
+        },
+      );
+    }
+
+    Future updateMhs() async {
+      final tempmhs = Mahasiswa(
+        id: mhs.id,
+        nim: txtnim.text,
+        namaLengkap: txtnama.text,
+        kelas: txtkelas.text,
+        prodi: txtprodi.text,
+      );
+      await MhsDatabase.instance.update(tempmhs);
+      Fluttertoast.showToast(
+          msg: "Data Berhasil Diubah",
+          toastLength: Toast.LENGTH_SHORT,
+          gravity: ToastGravity.CENTER,
+          timeInSecForIosWeb: 1,
+          fontSize: 16.0);
+    }
+
+    void onClose() {
+      Navigator.pop(context);
+    }
+
+    setText();
+    return Scaffold(
+      backgroundColor: Constants.primaryColor,
+      body: Container(
+        child: Stack(
+          clipBehavior: Clip.none,
+          children: [
+            Positioned(
+              right: 0.0,
+              top: 10.0,
+              child: Opacity(
+                opacity: 0.3,
+                child: Image.asset(
+                  "assets/images/washing_machine_illustration.png",
+                ),
+              ),
+            ),
+            SingleChildScrollView(
+              child: Container(
+                padding: EdgeInsets.symmetric(horizontal: 24.0),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    SizedBox(
+                      height: kToolbarHeight,
+                    ),
+                    GestureDetector(
+                      onTap: () {
+                        onClose();
+                      },
+                      child: Icon(
+                        FlutterIcons.keyboard_backspace_mdi,
+                        color: Colors.white,
+                      ),
+                    ),
+                    SizedBox(
+                      height: 20.0,
+                    ),
+                    RichText(
+                      text: TextSpan(
+                        children: [
+                          TextSpan(
+                            text: "Details Mahasiswa\n",
+                            style:
+                                Theme.of(context).textTheme.headline6.copyWith(
+                                      color: Colors.white,
+                                    ),
+                          ),
+                          TextSpan(
+                            text: mhs.namaLengkap,
+                            style:
+                                Theme.of(context).textTheme.headline6.copyWith(
+                                      color: Colors.white,
+                                      fontWeight: FontWeight.w600,
+                                    ),
+                          ),
+                        ],
+                      ),
+                    ),
+                    SizedBox(
+                      height: 40.0,
+                    ),
+                    Container(
+                      width: double.infinity,
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(8.0),
+                        color: Constants.scaffoldBackgroundColor,
+                      ),
+                      padding: EdgeInsets.symmetric(
+                        vertical: 24.0,
+                        horizontal: 16.0,
+                      ),
+                      child: Column(
+                        mainAxisSize: MainAxisSize.max,
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text(
+                            "Detail",
+                            style:
+                                Theme.of(context).textTheme.headline6.copyWith(
+                                      color: Color.fromRGBO(74, 77, 84, 1),
+                                      fontSize: 16.0,
+                                      fontWeight: FontWeight.w800,
+                                    ),
+                          ),
+                          SizedBox(
+                            height: 6.0,
+                          ),
+                          TextField(
+                            controller: txtnim,
+                            decoration: InputDecoration(
+                              border: OutlineInputBorder(),
+                              labelText: 'NIM',
+                              counterText: "",
+                            ),
+                            maxLength: 20,
+                            keyboardType: TextInputType.number,
+                            inputFormatters: <TextInputFormatter>[
+                              FilteringTextInputFormatter.allow(
+                                  RegExp(r'[0-9]')),
+                            ],
+                          ),
+                          SizedBox(
+                            height: 10.0,
+                          ),
+                          TextField(
+                            controller: txtnama,
+                            decoration: InputDecoration(
+                              border: OutlineInputBorder(),
+                              labelText: 'Nama Lengkap',
+                              counterText: "",
+                            ),
+                            maxLength: 40,
+                          ),
+                          SizedBox(
+                            height: 10.0,
+                          ),
+                          TextField(
+                            controller: txtkelas,
+                            decoration: InputDecoration(
+                              border: OutlineInputBorder(),
+                              labelText: 'Kelas',
+                              counterText: "",
+                            ),
+                            maxLength: 20,
+                          ),
+                          SizedBox(
+                            height: 10.0,
+                          ),
+                          TextField(
+                            controller: txtprodi,
+                            decoration: InputDecoration(
+                              border: OutlineInputBorder(),
+                              labelText: 'Prodi',
+                              counterText: "",
+                            ),
+                            maxLength: 25,
+                          ),
+                        ],
+                      ),
+                    ),
+                    SizedBox(height: 10.0),
+                    Container(
+                      decoration: BoxDecoration(
+                        color: Colors.white,
+                        borderRadius: BorderRadius.circular(8.0),
+                      ),
+                      padding: EdgeInsets.all(16.0),
+                      height: ScreenUtil().setHeight(150.0),
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.stretch,
+                        children: [
+                          ElevatedButton(
+                            style: ElevatedButton.styleFrom(
+                              primary: Constants.primaryColor,
+                            ),
+                            onPressed: () {
+                              updateMhs();
+                              onClose();
+                            },
+                            child: const Text('Ubah'),
+                          ),
+                          SizedBox(
+                            height: 5.0,
+                          ),
+                          TextButton.icon(
+                            icon: Icon(FlutterIcons.trash_2_fea),
+                            label: Text("Hapus"),
+                            style: TextButton.styleFrom(
+                                primary: Colors.red,
+                                textStyle:
+                                    TextStyle(fontWeight: FontWeight.bold)),
+                            onPressed: () async {
+                              _showMyDialog(
+                                  "Peringatan", "Yakin Data mau Dihapus?");
+                            },
+                          ),
+                        ],
+                      ),
+                    )
+                  ],
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+Widget getTotalRow(String title, String amount) {
+  return Padding(
+    padding: EdgeInsets.only(bottom: 8.0),
+    child: Row(
+      children: [
+        Text(
+          title,
+          style: TextStyle(
+            color: Color.fromRGBO(19, 22, 33, 1),
+            fontSize: 17.0,
+            fontWeight: FontWeight.w600,
+          ),
+        ),
+        Spacer(),
+        Text(
+          amount,
+          style: TextStyle(
+            color: Constants.primaryColor,
+            fontWeight: FontWeight.w600,
+            fontSize: 17.0,
+          ),
+        )
+      ],
+    ),
+  );
+}
+
+Widget getSubtotalRow(String title, String price) {
+  return Padding(
+    padding: EdgeInsets.only(bottom: 8.0),
+    child: Row(
+      children: [
+        Text(
+          title,
+          style: TextStyle(
+            color: Color.fromRGBO(74, 77, 84, 1),
+            fontSize: 15.0,
+            fontWeight: FontWeight.w600,
+          ),
+        ),
+        Spacer(),
+        Text(
+          price,
+          style: TextStyle(
+            color: Color.fromRGBO(74, 77, 84, 1),
+            fontSize: 15.0,
+          ),
+        )
+      ],
+    ),
+  );
+}
+
+Widget getItemRow(String count, String item, String price) {
+  return Padding(
+    padding: EdgeInsets.only(bottom: 8.0),
+    child: Row(
+      children: [
+        Text(
+          count,
+          style: TextStyle(
+            color: Color.fromRGBO(74, 77, 84, 1),
+            fontSize: 15.0,
+            fontWeight: FontWeight.w600,
+          ),
+        ),
+        Expanded(
+          child: Text(
+            " x $item",
+            style: TextStyle(
+              color: Color.fromRGBO(143, 148, 162, 1),
+              fontSize: 15.0,
+            ),
+          ),
+        ),
+        Text(
+          price,
+          style: TextStyle(
+            color: Color.fromRGBO(74, 77, 84, 1),
+            fontSize: 15.0,
+          ),
+        )
+      ],
+    ),
+  );
+}
---
+++ lib/pages/login.dart
@@ -0,0 +1,226 @@
+import 'package:flutter/material.dart';
+import 'package:logbook_management/Animation/FadeAnimation.dart';
+import 'package:logbook_management/utils/helper.dart';
+import 'package:rounded_loading_button/rounded_loading_button.dart';
+import 'dart:async';
+import 'package:fluttertoast/fluttertoast.dart';
+
+class Login extends StatefulWidget {
+  @override
+  _LoginState createState() => _LoginState();
+}
+
+class _LoginState extends State<Login> {
+  final RoundedLoadingButtonController _btnController2 =
+      RoundedLoadingButtonController();
+
+  Future<void> _showMyDialog(String _tittle, String _msg) async {
+    return showDialog<void>(
+      context: context,
+      barrierDismissible: false, // user must tap button!
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: Text(_tittle),
+          content: SingleChildScrollView(
+            child: ListBody(
+              children: <Widget>[
+                Text(_msg),
+              ],
+            ),
+          ),
+          actions: <Widget>[
+            TextButton(
+              child: const Text('OK'),
+              onPressed: () {
+                Navigator.of(context).pop();
+              },
+            ),
+          ],
+        );
+      },
+    );
+  }
+
+  void _doSomething(RoundedLoadingButtonController controller) async {
+    Timer(Duration(seconds: 2), () {
+      chekUser(cusername, cpassword);
+      if (statusLog) {
+        controller.success();
+        nextScreen(context, "/Home");
+      } else {
+        controller.error();
+      }
+      Timer(Duration(seconds: 2), () {
+        _btnController2.reset();
+      });
+    });
+  }
+
+  final cusername = TextEditingController();
+  final cpassword = TextEditingController();
+  var statusLog = false;
+
+  void chekUser(TextEditingController _user, _pass) async {
+    if (_user.text == "" && _pass.text == "") {
+      _showMyDialog("Peringatan", "Username/Password masih kosong");
+      statusLog = false;
+    } else if (_user.text != "admin" && _pass.text != "admin") {
+      _showMyDialog("Peringatan", "username/password salah");
+      statusLog = false;
+    } else if (_user.text == "admin" && _pass.text == "admin") {
+      statusLog = true;
+      Fluttertoast.showToast(
+          msg: "Selamat Datang Kembali, Admin",
+          toastLength: Toast.LENGTH_LONG,
+          gravity: ToastGravity.CENTER,
+          timeInSecForIosWeb: 1,
+          fontSize: 16.0);
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: Container(
+          height: double.infinity,
+          decoration: BoxDecoration(
+              image: DecorationImage(
+            image: AssetImage('assets/images/background-dark.png'),
+            fit: BoxFit.fill,
+          )),
+          child: SingleChildScrollView(
+            child: Column(
+              children: <Widget>[
+                FadeAnimation(
+                  1,
+                  Container(
+                    height: 400,
+                    decoration: BoxDecoration(
+                        image: DecorationImage(
+                      image: AssetImage('assets/images/Vector2.png'),
+                      fit: BoxFit.fill,
+                    )),
+                    child: Stack(
+                      children: <Widget>[
+                        Container(
+                          decoration: BoxDecoration(
+                              image: DecorationImage(
+                            image: AssetImage('assets/images/Vector1.png'),
+                            fit: BoxFit.fill,
+                          )),
+                        ),
+                        Positioned(
+                          child: Center(
+                            child: Container(
+                              margin: EdgeInsets.only(bottom: 100),
+                              decoration: BoxDecoration(
+                                  image: DecorationImage(
+                                      image: AssetImage(
+                                          'assets/images/logo_unikom.png'))),
+                            ),
+                          ),
+                        ),
+                        Positioned(
+                          child: Container(
+                            margin: EdgeInsets.only(top: 100),
+                            child: Center(
+                              child: Text(
+                                "Log Book Management\n System 2021",
+                                textAlign: TextAlign.center,
+                                style: TextStyle(
+                                  color: Colors.white,
+                                  fontSize: 20,
+                                ),
+                              ),
+                            ),
+                          ),
+                        )
+                      ],
+                    ),
+                  ),
+                ),
+                Padding(
+                  padding: EdgeInsets.all(30.0),
+                  child: Column(
+                    children: <Widget>[
+                      FadeAnimation(
+                          1.8,
+                          Container(
+                            padding: EdgeInsets.all(5),
+                            decoration: BoxDecoration(
+                                color: Colors.white,
+                                borderRadius: BorderRadius.circular(10),
+                                boxShadow: [
+                                  BoxShadow(
+                                      color: Color.fromRGBO(143, 148, 251, .2),
+                                      blurRadius: 20.0,
+                                      offset: Offset(0, 10))
+                                ]),
+                            child: Column(
+                              children: <Widget>[
+                                Container(
+                                  padding: EdgeInsets.all(8.0),
+                                  decoration: BoxDecoration(
+                                      border: Border(
+                                          bottom: BorderSide(
+                                              color: Colors.grey[100]))),
+                                  child: TextField(
+                                    controller: cusername,
+                                    decoration: InputDecoration(
+                                        border: InputBorder.none,
+                                        hintText: "Username",
+                                        hintStyle:
+                                            TextStyle(color: Colors.grey[400])),
+                                  ),
+                                ),
+                                Container(
+                                  padding: EdgeInsets.all(8.0),
+                                  child: TextField(
+                                    controller: cpassword,
+                                    autofocus: false,
+                                    obscureText: true,
+                                    decoration: InputDecoration(
+                                        border: InputBorder.none,
+                                        hintText: "Password",
+                                        hintStyle:
+                                            TextStyle(color: Colors.grey[400])),
+                                  ),
+                                )
+                              ],
+                            ),
+                          )),
+                      SizedBox(
+                        height: 30,
+                      ),
+                      FadeAnimation(
+                        2,
+                        RoundedLoadingButton(
+                          color: Colors.indigo[600],
+                          successColor: Colors.green[800],
+                          controller: _btnController2,
+                          onPressed: () => _doSomething(_btnController2),
+                          borderRadius: 8,
+                          child: Text('Login',
+                              style: TextStyle(color: Colors.white)),
+                        ),
+                      ),
+                      SizedBox(
+                        height: 20,
+                      ),
+                      FadeAnimation(
+                          1.5,
+                          Text(
+                            "Logout",
+                            style: TextStyle(
+                                color: Color.fromRGBO(143, 148, 251, 1)),
+                          )),
+                    ],
+                  ),
+                ),
+              ],
+            ),
+          )),
+    );
+  }
+}
