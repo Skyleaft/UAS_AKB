eb407677c1c5bc9d1bc2f2e71f74161147f140f9
Milzan Malik <milzan_malik@outlook.com>
2021-08-02 18:26:50 +0700

    fix login#2 new launcher

--- lib/pages/login.dart
+++ lib/pages/login.dart
@@ -81,16 +81,16 @@ class _LoginState extends State<Login> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        backgroundColor: Colors.white,
-        body: SingleChildScrollView(
-            child: Column(children: <Widget>[
+      backgroundColor: Colors.white,
+      body: SingleChildScrollView(
+        child: Column(children: <Widget>[
           Stack(
             children: [
               Positioned(
                 child: FadeAnimation(
                   1,
                   Container(
-                    height: 600,
+                    height: 700,
                     child: Align(
                         alignment: Alignment.topRight,
                         child: Image.asset('assets/images/Vector.png')),
@@ -116,62 +116,66 @@ class _LoginState extends State<Login> {
                   1,
                   Container(
                     child: Align(
-                        alignment: Alignment.topLeft,
-                        child: Image.asset('assets/images/Vector2.png')),
+                      alignment: Alignment.topLeft,
+                      child: Image.asset('assets/images/Vector2.png'),
+                    ),
                   ),
                 ),
               ),
               Positioned(
+                top: 350,
+                left: 0,
+                right: 0,
                 child: Padding(
                   padding: EdgeInsets.all(30),
                   child: Column(
                     children: <Widget>[
-                      FadeAnimation(
-                          1.8,
-                          Container(
-                            padding: EdgeInsets.all(5),
-                            decoration: BoxDecoration(
-                                color: Colors.white,
-                                borderRadius: BorderRadius.circular(10),
-                                boxShadow: [
-                                  BoxShadow(
-                                      color: Color.fromRGBO(143, 148, 251, .2),
-                                      blurRadius: 20.0,
-                                      offset: Offset(0, 10))
-                                ]),
-                            child: Column(
-                              children: <Widget>[
-                                Container(
-                                  padding: EdgeInsets.all(8.0),
-                                  decoration: BoxDecoration(
-                                      border: Border(
-                                          bottom: BorderSide(
-                                              color: Colors.grey[100]))),
-                                  child: TextField(
-                                    controller: cusername,
-                                    decoration: InputDecoration(
-                                        border: InputBorder.none,
-                                        hintText: "Username",
-                                        hintStyle:
-                                            TextStyle(color: Colors.grey[400])),
-                                  ),
-                                ),
-                                Container(
-                                  padding: EdgeInsets.all(8.0),
-                                  child: TextField(
-                                    controller: cpassword,
-                                    autofocus: false,
-                                    obscureText: true,
-                                    decoration: InputDecoration(
-                                        border: InputBorder.none,
-                                        hintText: "Password",
-                                        hintStyle:
-                                            TextStyle(color: Colors.grey[400])),
-                                  ),
-                                )
-                              ],
+                      Container(
+                        padding: EdgeInsets.all(5),
+                        decoration: BoxDecoration(
+                            color: Colors.white,
+                            borderRadius: BorderRadius.circular(10),
+                            boxShadow: [
+                              BoxShadow(
+                                  color: Color.fromRGBO(143, 148, 251, .2),
+                                  blurRadius: 20.0,
+                                  offset: Offset(0, 10))
+                            ]),
+                        child: Column(
+                          children: <Widget>[
+                            Container(
+                              width: 300,
+                              padding: EdgeInsets.all(8.0),
+                              decoration: BoxDecoration(
+                                  border: Border(
+                                      bottom:
+                                          BorderSide(color: Colors.grey[100]))),
+                              child: TextField(
+                                controller: cusername,
+                                decoration: InputDecoration(
+                                    border: InputBorder.none,
+                                    hintText: "Username",
+                                    hintStyle:
+                                        TextStyle(color: Colors.grey[400])),
+                              ),
                             ),
-                          )),
+                            Container(
+                              width: 300,
+                              padding: EdgeInsets.all(8.0),
+                              child: TextField(
+                                controller: cpassword,
+                                autofocus: false,
+                                obscureText: true,
+                                decoration: InputDecoration(
+                                    border: InputBorder.none,
+                                    hintText: "Password",
+                                    hintStyle:
+                                        TextStyle(color: Colors.grey[400])),
+                              ),
+                            )
+                          ],
+                        ),
+                      ),
                       SizedBox(
                         height: 30,
                       ),
@@ -190,12 +194,31 @@ class _LoginState extends State<Login> {
                       SizedBox(
                         height: 20,
                       ),
+                      FadeAnimation(
+                          1.5,
+                          Text(
+                            "Lupa Password",
+                            style: TextStyle(
+                                color: Color.fromRGBO(143, 148, 251, 1)),
+                          )),
                     ],
                   ),
                 ),
-              )
+              ),
             ],
           ),
-        ])));
+          Container(
+            height: 50,
+            child: Align(
+              alignment: Alignment(-1, 1),
+              child: Container(
+                height: 30,
+                child: Image.asset('assets/images/logo-mz3.png'),
+              ),
+            ),
+          ),
+        ]),
+      ),
+    );
   }
 }
